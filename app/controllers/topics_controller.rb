a904d9bb999a73e164ba6589a47b5d4f4bc55abd
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2015-02-04 18:56:09 +0000

    Finished CRUD on topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,18 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:title))
+    if @topic.save
+      flash[:notice] = "New Topic was saved."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error saving the topic."
+      render :new
+    end
   end
 
   def show
@@ -12,7 +24,31 @@ class TopicsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
 
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(params.require(:topic).permit(:title))
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error updating the topic. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    name = @topic.title
+
+    if @topic.destroy
+      flash[:notice] = "\"#{name}\" was deleted."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic"
+      render :show
+    end
+  end
 
 end

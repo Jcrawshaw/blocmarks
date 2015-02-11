e8aad68c8566ddf1be776b49eebdf3fa005780b9
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2015-02-11 16:49:12 +0000

    Worked on incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,13 +3,17 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    logger.info "IncomingController#create: params: #{params.inspect}"
 
     @user = User.find_by_email(params[:sender])
-    @topic = Topic.find_by_subject(params[:subject])
+    # todo: if @user.nil? then create a new user
+    @topic = Topic.find_by_title(params[:subject])
+    # todo: unless @topic, create the topic
     @url = params["body-plain"]
 
 
-    @bookmark= Bookmark.create(url: @url)
+    @bookmark= Bookmark.build(url: @url)
+    @bookmark.save
     
 
     head 200

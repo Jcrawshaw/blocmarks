3aeac5defbfc7cc80e7a8973d2be2a38b5f17db3
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2015-02-24 17:42:44 +0000

    Configured Embedly gem

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,26 @@
+require 'embedly'
+
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+
+  def thumbnail
+    @thumbnail ||= begin
+      obj = Bookmark.embedly.oembed url: url
+      obj.first.thumbnail_url
+    end
+    @thumbnail || default_thumbnail
+  end
+
+  def default_thumbnail
+    "http://images.apple.com/home/images/og.jpg?201502111511"
+  end
+
+  def self.embedly
+    # memoize
+    @embedly ||= Embedly::API.new user_agent:  'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)', key: 'a8fe13a0b26447ee9df8758e5a2259a9'
+  end
+
 end
